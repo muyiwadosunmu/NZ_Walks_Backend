2542c7b28ac0a12460a295e44ea32c87dca779e7
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-11-06 15:23:18 +0100

    Implemented DTOs -> Data Transfer Objects

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks_API.Data;
 using NZWalks_API.Models.Domain;
+using NZWalks_API.Models.Domain.DTOs;
 
 namespace NZWalks_API.Controllers
 {
@@ -12,25 +14,59 @@ namespace NZWalks_API.Controllers
     [Route("api/[controller]")]
     public class RegionsController : ControllerBase
     {
+        private readonly NZ_Walks_DB_Context _dbContext;
+        public RegionsController(NZ_Walks_DB_Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        /*GET ALL REGIONS*/
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region> {
-                new() {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://www.pexels.com/photo/the-lightpath-in-the-middle-of-the-freeway-in-auckland-5342974/"
-                },
-                new() {
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington Region",
-                    Code = "WLG",
-                    RegionImageUrl = "https://www.pexels.com/photo/the-sun-is-setting-over-a-city-and-mountains-18062808/"
-                }
+            // Get Data From Database - Domain models
+            var regionsDomain = _dbContext.Regions.ToList();
+
+            // Map Domain Models to DTO
+            var regionsDto = new List<RegionDTO>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDTO()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+
+                });
+            }
+            return Ok(regionsDto);
+        }
+
+        /* GET SINGLE REGION (GET Region by Id*/
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // Get Data From Database - Domain models
+            var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            // var region = _dbContext.Regions.Find(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            // Map/Convert RegionDomain to RegionDto
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
             };
 
-            return Ok(regions);
+            // Return DTO back to client
+            return Ok(regionDto);
         }
+
+
     }
 }
---
+++ Models/DTOs/RegionDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NZWalks_API.Models.Domain.DTOs
+{
+    public class RegionDTO
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+
+    }
+}
